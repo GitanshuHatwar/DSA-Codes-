ab7029dd5ca0452746c12e2bc9f023ccfc5a5fce
GitanshuHatwar <gitanshuhatwar@gmail.com>
2025-04-27 12:14:43 +0530

    basic test mingw and changes in git

---
+++ newone.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
+// This is the basic code for c++ program. It includes the iostream library, which is used for input and output operations. The main function is the entry point of the program, and it prints "Hello, World!" to the console using cout. Finally, it returns 0 to indicate successful execution
